d444477c95daa9c4776d4707506baae150e0a12c
Harold <harol.mont97@gmail.com>
2019-10-15 22:44:04 -0500

    Se realizan cambios sobre todo el modulo de autenticacion

---
+++ letgo-party-bck/src/routes/enroll.routes.js
@@ -0,0 +1,11 @@
+module.exports = function(app){
+    const Prof2USR = require("../controllers/prof2usr.controller")
+    const Rol = require("../controllers/rol.controller")
+    const Profile = require("../controllers/profile.controller")
+
+    app.post("/api/profile", Profile.createProfile)
+
+    app.post("/api/rol", Rol.createRol)
+
+    app.post("/api/profiletouser", Prof2USR.associateUsrToProfile)
+}
